400a9faeac6775989b2e3f23562bb85eb7ed5ff7
Dheeraj Gopinath <dheerajgopinath@gmail.com>
2016-07-19 19:49:38 +0530

    add query function to fetch weather info from openweather api

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "net/http"
+import (
+    "net/http"
+    "encoding/json"
+)
+
+type weatherData struct {
+    Name string `json:"name"`
+    Main struct {
+        Kelvin float64 `json:"temp"`
+    } `json:"main"`
+}
 
 func main() {
     http.HandleFunc("/", hello)
@@ -9,4 +19,21 @@ func main() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello!"))
+}
+
+func query(city string) (weatherData, error) {
+    resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=b772eefc824c6bf22bba33f8fada5ed6&q=" + city)
+    if err != nil {
+        return weatherData{}, err
+    }
+
+    defer resp.Body.Close()
+
+    var weatherInfo weatherData
+
+    if err := json.NewDecoder(resp.Body).Decode(&weatherInfo); err != nil {
+        return weatherData{}, err
+    }
+
+    return weatherInfo, nil
 }
